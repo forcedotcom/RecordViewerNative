d5fe5a106de8d7e9596ea7e69c81ced9aa9077c0
jbleyleSF <32077211+jbleyleSF@users.noreply.github.com>
2017-10-19 09:45:08 -0700

    Updated callback url

--- Config.js
+++ Config.js
@@ -3,7 +3,7 @@
 const Config = {
   loginUrl: 'https://login.salesforce.com/services/oauth2/authorize?',
   consumerKey: '{your-oauth-consumer-key}',
-  oauthRedirect: 'https://{your-domain}/oauth-redirect'
+  oauthRedirect: 'https://'
 }
 
 export default Config
